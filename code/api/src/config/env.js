c4de3ac5dd1e36c9746da9eb981a3fea0026025a
Dan Sehl <dan.sehl@gmail.com>
2020-10-10 16:37:46 -0600

    Add config/env file annotations

--- code/api/src/config/env.js
+++ code/api/src/config/env.js
@@ -9,3 +9,5 @@ export const NODE_ENV = process.env.NODE_ENV
 
 // Port
 export const PORT = process.env.PORT || 8000
+
+// It looks like this file is similar to the Rails file config/application.yml. Though it is not .gitignored. This is the file where you would store environment variables to be called in other parts of the app.
