1d4ef9116cbe2124a1a6605a8cf3dba429f250f2
Jake West <jacobkwest93@gmail.com>
2020-10-11 15:58:05 -0600

    Add annotations to Profile/user files #15

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -1,3 +1,7 @@
+// Profile.js is the component rendered at rootPath/user/profile. Right now, this is where the user views their profile info (name/email), logs out, as well as sees their subscriptions. Because of the nature of our track, we'll be doing some updating to this component to display the users picture, profile info, and order history. 
+
+// We'll need to add state to the Profile to allow some components to function as a controlled form. We'll import most of the input types from ui/inputs (File for image, text for email/name/address, input type="date" for days available). We'll need to create an onChange event & onSubmit event, which should update the user in both the database, set the new user & trigger a page re-render. OR we build a new component (ProfileForm) which has all of this functionality (I like this latter option- it feels more SRP)
+
 // Imports
 import React from 'react'
 import PropTypes from 'prop-types'
@@ -15,7 +19,7 @@ import { grey, grey2 } from '../../ui/common/colors'
 import userRoutes from '../../setup/routes/user'
 import { logout } from './api/actions'
 
-// Component
+// Component: JSX for our Profile component- we'll need to build off of this to display user info.
 const Profile = (props) => (
   <div>
     {/* SEO */}
@@ -46,17 +50,18 @@ const Profile = (props) => (
   </div>
 )
 
-// Component Properties
+// Component Properties: acts as a safeguard to make sure that the Profile component's props are data types we expect. If we add a updateProfile method, we'll need to add PropTypes.func.isRequired for it.
 Profile.propTypes = {
   user: PropTypes.object.isRequired,
   logout: PropTypes.func.isRequired
 }
 
-// Component State
+// Component State: this is acting as our mapStateToProps- it provides this component with the user object from store { error: '', isAuthenticated: false, isLoading: false, details: {} }. Assuming we add profile details (photo, name, email, shipping address, etc) to the user.details, this info will automatically be linked to our component and can be rendered.
 function profileState(state) {
   return {
     user: state.user
   }
 }
 
+// This function connects our Profile component to props defined by profileState (user) & to the method `logout` from the user/api/actions. As we connect more functionality & have more methods called when the user updates their profile, we'll need to add them to this mapDispatch object.
 export default connect(profileState, { logout })(Profile)
--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -2,7 +2,7 @@
 import { isEmpty } from '../../../setup/helpers'
 import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
 
-// Initial State
+// Initial State: defines initial state for parts of store related to user. This is the info in store at @@Init in dev tools.
 export const userInitialState = {
   error: null,
   isLoading: false,
@@ -10,7 +10,25 @@ export const userInitialState = {
   details: null
 }
 
-// State
+// State: This is our user reducer- it takes in our action object & returns a specific piece of store depending on the action type. Every piece of store is governed by it's own reducer.
+
+//For one of our features, we need to display all of the items a user has received and the ones they've kept. We could add a new property for history to the user.details object, which will have a value of an object that has product ids as keys & true/false as values for if the item was kept/returned.
+
+/* 
+- Example object for history- all items listen (by id) keys & kept items can be determined by true/false values.
+{ 
+  email: test@test.com,
+  history: {
+    1: true,
+    2: true,
+    3: false,
+    4: true,
+  }
+}
+
+- We'll need an action that updates this history, as the user selects/unselects items they are keeping.
+*/
+
 export default (state = userInitialState, action) => {
   switch (action.type) {
     case SET_USER:
