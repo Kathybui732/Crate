8679ef41a871301066a742482df63cdcee36d1ab
Dan Sehl <dan.sehl@gmail.com>
2020-10-12 20:12:42 -0600

    Add failing test for user update migration

---
+++ code/api/tests/user_update_mutation.test.js
@@ -0,0 +1,9 @@
+//
+// const update = require('../src/modules/user/mutations.js');
+//
+// test('returns updated user information response', () => {
+//   // expect(update(1, 2)).toBe(3);
+//   expect(update.type).toBe(UserType);
+// });
+
+// Note: BE has unproductively struggled on this for a while, not knowing how to write a test but feeling like we can write the code.
