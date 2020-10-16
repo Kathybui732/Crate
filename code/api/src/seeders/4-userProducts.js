b2aba6e13b94c771082fa0a0642540b08dcc7ede
Kathy Bui <kathy.bui1992@gmail.com>
2020-10-15 21:09:51 -0600

    Update userProduct Seed file to have status

--- code/api/src/seeders/4-userProducts.js
+++ code/api/src/seeders/4-userProducts.js
@@ -6,48 +6,56 @@ module.exports = {
       {
         userId: '2',
         productId: '1',
+        status: 'RETURNED',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: '2',
         productId: '2',
+        status: 'KEPT',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: '2',
         productId: '3',
+        status: 'RETURNED',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: '2',
         productId: '4',
+        status: 'KEPT',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: '2',
         productId: '5',
+        status: 'RETURNED',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: '2',
         productId: '6',
+        status: 'KEPT',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: '2',
         productId: '7',
+        status: 'RETURNED',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         userId: '2',
         productId: '8',
+        status: 'KEPT',
         createdAt: new Date(),
         updatedAt: new Date()
       }
