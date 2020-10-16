b727486dc985f59c2aa5047286a95861b111091d
Jake West <jacobkwest93@gmail.com>
2020-10-16 11:35:22 -0600

    Fix login fields

--- code/web/src/modules/user/api/actions.js
+++ code/web/src/modules/user/api/actions.js
@@ -36,11 +36,7 @@ export function login(userCredentials, isLoading = true) {
     return axios.post(routeApi, query({
       operation: 'userLogin',
       variables: userCredentials,
-<<<<<<< HEAD
       fields: ['user {name, email, role, image, id}', 'token']
-=======
-      fields: ['user {name, email, role, id}', 'token']
->>>>>>> Add order history
     }))
       .then(response => {
         let error = ''
@@ -141,16 +137,9 @@ export function updateUser(user, id) {
       image: user.imgURL,
     },
     })
-    
-// Get user
-export function getUser() {
-  return dispatch => {
-    return axios.post(routeApi, query({
-      operation: 'user',
-      fields: ['id', 'name', 'email', 'image', 'role', /* add rest of user fields here*/]
-    }))
   }
-}  
+}
+
 // Get single user
 export function getUser(id) {
   return dispatch => {
