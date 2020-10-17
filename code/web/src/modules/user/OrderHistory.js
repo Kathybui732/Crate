82a834be9080851ac3457dc0f274a68f38c9ab50
Jake West <jacobkwest93@gmail.com>
2020-10-16 18:03:22 -0600

    Connect OrderHistory to store & render products #37

--- code/web/src/modules/user/OrderHistory.js
+++ code/web/src/modules/user/OrderHistory.js
@@ -1,5 +1,6 @@
 // Imports
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 
@@ -14,35 +15,43 @@ import Button from '../../ui/button'
 import { routeImage } from '../../setup/routes'
 
 // Component
-const OrderHistory = (props) => (
-    <div>
-        {/* SEO */}
-        <Helmet>
-            <title>My Profile - Order History - Crate</title>
-        </Helmet>
+const OrderHistory = (props) => {
+    return (
+        <div>
+            {/* SEO */}
+            <Helmet>
+                <title>My Profile - Order History - Crate</title>
+            </Helmet>
 
-        {/* Grid of products */}
-        <Grid style={{ marginLeft: '10%', marginRight: '10%' }}>
-            {props.products.map(product => (
-                <GridCell key={product.id} style={{ textAlign: 'center' }}>
-                    <Card style={{ width: '15em', margin: '2.5em auto', backgroundColor: white, paddingBottom: '1em' }}>
-                        <img src={routeImage + product.image} alt={product.name} style={{ width: '100%' }} />
+            {/* Grid of products */}
+            <Grid style={{ marginLeft: '10%', marginRight: '10%' }}>
+                {props.products.map(product => (
+                    <GridCell key={product.product.id} style={{ textAlign: 'center' }}>
+                        <Card style={{ width: '15em', margin: '2.5em auto', backgroundColor: white, paddingBottom: '1em' }}>
+                            <img src={routeImage + product.product.image} alt={product.product.name} style={{ width: '100%' }} />
 
-                        <div style={{ padding: '1em 1.2em' }}>
-                            <H4 font="secondary" style={{ color: black }}>{product.name}</H4>
+                            <div style={{ padding: '1em 1.2em' }}>
+                                <H4 font="secondary" style={{ color: black }}>{product.product.name}</H4>
 
-                            <p style={{ color: grey2, marginTop: '1em' }}>{product.description}</p>
-                        </div>
-                        <Button theme='primary' onClick={() => {console.log(`Keep item ${product.id}`)} }>Keep Item</Button>
-                    </Card>
-                </GridCell>
-            ))}
-        </Grid>
-    </div>
-)
+                                <p style={{ color: grey2, marginTop: '1em' }}>{product.product.description}</p>
+                            </div>
+                            <Button theme='primary' onClick={() => {console.log(`Keep item ${product.product.id}`)} }>Keep Item</Button>
+                        </Card>
+                    </GridCell>
+                ))}
+            </Grid>
+        </div>
+    )
+}
 
 OrderHistory.propTypes = {
     products: PropTypes.array
 }
 
-export default OrderHistory
+function orderHistoryState(state) {
+    return {
+        products: state.user.userProducts,
+    }
+}
+
+export default connect(orderHistoryState)(OrderHistory)
